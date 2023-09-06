7a71bee6ae075c5caed8be4adc0d076de8f157ed
Omar <omar.yassermarei@live.com>
2023-09-05 17:57:36 -0700

    Added string formatting instead of f formatting.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 90):
-    if (int(str(f"{i:02}")[0]) < int(str(f"{i:02}")[1])):
-        if (int(str(f"{i:02}")[0]) != int(str(f"{i:02}")[1])):
-            if (i != 89):
-                print(f"{i:02},", end=" ")
-            else:
-                print(f"{i:02}")
+    str1 = str("{:02d}".format(i))[0]
+    str2 = str("{:02d}".format(i))[1]
+    if (int(str1) < int(str2) and int(str1) != int(str2)):
+        if (i != 89):
+            print("{:02d},".format(i), end=" ")
+        else:
+            print("{:02d}".format(i))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for i in range(0, length(str)):
         if (i != length(str) - 1):
-            print(chr(ord(str[i]) - 32), end="")
+            print("{}".format(chr(ord(str[i]) - 32)), end="")
         else:
-            print(chr(ord(str[i]) - 32))
+            print("{}".format(chr(ord(str[i]) - 32)))
