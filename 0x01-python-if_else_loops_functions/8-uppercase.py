0defe37571e639c1c7452766bb866bc95bc3a1cb
Omar <omar.yassermarei@live.com>
2023-09-05 18:01:16 -0700

    Changed built in string in task 8.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for i in range(0, length(str)):
+    for i in range(0, len(str)):
         if (i != length(str) - 1):
             print("{}".format(chr(ord(str[i]) - 32)), end="")
         else:
