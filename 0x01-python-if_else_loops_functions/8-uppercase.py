dd52965431b2882ddb9d20ca3158741bfdd4a02e
Omar <omar.yassermarei@live.com>
2023-09-05 18:14:30 -0700

    Adjusted for edge cases in task 8.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,21 @@
 
 def uppercase(str):
     for i in range(0, len(str)):
-        if (i != len(str) - 1):
-            print("{}".format(chr(ord(str[i]) - 32)), end="")
+
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+
+            if (i != len(str) - 1):
+                delim = ""
+            else:
+                delim = "\n"
+
+            print("{}".format(chr(ord(str[i]) - 32)), end=delim)
+
         else:
-            print("{}".format(chr(ord(str[i]) - 32)))
+
+            if (i != len(str) - 1):
+                delim = ""
+            else:
+                delim = "\n"
+
+            print("{}".format(str[i]), end=delim)
