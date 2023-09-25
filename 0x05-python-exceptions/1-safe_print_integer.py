06b8cc41bd14ffbf96d2676161c75050670a2ae6
Omar <omar.yassermarei@live.com>
2023-09-25 04:21:43 -0700

    Adjusted syntax error.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     flag = True
     try:
-        print({:d}.format(value), end="\n")
+        print("{:d}".format(value))
     except TypeError:
         flag = False
 
