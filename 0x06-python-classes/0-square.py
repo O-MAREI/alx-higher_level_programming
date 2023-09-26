55752f598b51c4a5b2f1dd90910d5882c69b4cf9
Omar <omar.yassermarei@live.com>
2023-09-26 14:13:39 -0700

    Mandatory tasks added.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+class Square:
+    """A class to represent a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """A class to represent a square."""
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the new square.
+        """
+        self.__size = size
