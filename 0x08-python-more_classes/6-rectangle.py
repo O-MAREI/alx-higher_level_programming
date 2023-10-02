c86dc39d41178512c6523afef9666aba419d3929
Omar <omar.yassermarei@live.com>
2023-10-02 06:50:12 -0700

    Reverted back to previous commit.

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -34,7 +34,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -35,7 +35,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
