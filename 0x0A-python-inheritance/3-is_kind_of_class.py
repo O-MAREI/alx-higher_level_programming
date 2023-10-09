06fbc3978f18ef26ffd52a899efca9ce02c4c496
Omar <omar.yassermarei@live.com>
2023-10-09 06:26:10 -0700

    Adjusted syntax error in task 3.

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """ Module 3 - is_kind_of_class """
 
 
-def is_kind_of__class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """ Returns True if obj is an instance of a_class
     or any of its subclasses, False if not. """
 
